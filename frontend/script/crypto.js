29f4e314e5b60fb2f6dd558bb596362bc88fa8b0
sumit9235 <sy7985494155@gmail.com>
2023-05-05 17:42:53 +0530

    added chat application to crypto.js

--- frontend/script/crypto.js
+++ frontend/script/crypto.js
@@ -78,6 +78,39 @@ var chart = new Chart(ctx, {
             chart.update();
           }
         }, 1000);
+// for socket .io
+const socket = io("http://localhost:8000/", { transports: ["websocket"] });
+     document.querySelector("#form").addEventListener("submit",(e)=>{
+        e.preventDefault();
+        let name=localStorage.getItem("user");
+        let msg = document.querySelector("#message").value;
+        sent_msg(name,msg)
+        document.querySelector("#message").value = '';
+     })
+
+     function sent_msg(name,msg){
+        socket.emit("chat",name,msg)
+     }
+     
+     
+     socket.on("recived_msg",(name,msg)=>{
+      let div = document.querySelector(".message-box");
+      let p = document.createElement("p");
+      let nameSpan = document.createElement("span");
+      let msgSpan = document.createElement("span");
+      
+      nameSpan.textContent = `${name}: `;
+      nameSpan.className = "name";
+      
+      msgSpan.textContent = msg;
+      msgSpan.className = "msg";
+      
+      p.appendChild(nameSpan);
+      p.appendChild(msgSpan);
+      
+      div.appendChild(p);
+    })
+
       
       
     
